2063601b96684defd0cce60367a6159ab1294b0c
goodspb <goodspb@qq.com>
2016-05-27 09:07:54 +0800

    feature: view DIY functions can be define in config file

--- app/common/View.php
+++ app/common/View.php
@@ -12,13 +12,21 @@ class View
         $viewConfig = Config::get('view');
         $di->set('voltService', function ($view, $di) use ($viewConfig) {
             $volt = new Volt($view, $di);
-            is_dir($viewConfig['volt']['compiledPath']) or mkdir($viewConfig['volt']['compiledPath']);
+            is_dir($viewConfig['volt']['compiledPath']) or mkdir($viewConfig['volt']['compiledPath'], 0777, true);
+
+            if (Config::get('app.debug', false)) {
+                $viewConfig['volt']['compileAlways'] = true;
+            }
             $volt->setOptions($viewConfig['volt']);
 
             //自定义 volt 模板方法
             $compiler = $volt->getCompiler();
-            //多语言支持
-            $compiler->addFunction('lang', 'lang');
+            //自定义方法
+            if (!empty($functions = $viewConfig['functions'])) {
+                foreach ($functions as $name => $definition) {
+                    $compiler->addFunction($name, $definition);
+                }
+            }
 
             return $volt;
         });
--- app/config/view.php
+++ app/config/view.php
@@ -7,5 +7,15 @@ return [
         "compiledPath" => ROOT_PATH . "/storage/templates/",
         "compiledExtension" => ".compiled.php"
     ],
-
+    'functions' => [
+        'config' => function ($resolvedArgs, $exprArgs) {
+            return '\Config::get(' . $resolvedArgs . ')';
+        },
+        'session' => function ($resolvedArgs, $exprArgs) {
+            return '\Session::get(' . $resolvedArgs . ')';
+        },
+        'lang' => function ($resolvedArgs, $exprArgs) {
+            return '\Lang::get(' . $resolvedArgs . ')';
+        },
+    ],
 ];
